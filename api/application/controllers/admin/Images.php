a8b2a49cebbfaf628ec77f3c9fd7dd3c77250a98
Rafal Jankowski <rafal.jankowski.93@gmail.com>
2016-07-09 17:01:53 +0200

    upgrade

---
+++ api/application/controllers/admin/Images.php
@@ -0,0 +1,39 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Images extends CI_Controller {
+
+	public function upload( $id )
+	{
+        echo 'No files';
+if ( !empty( $_FILES ) )
+		{
+		    $tempPath = $_FILES[ 'file' ][ 'tmp_name' ];
+
+		    $basePath = FCPATH . '..' . DIRECTORY_SEPARATOR . 'uploads' . DIRECTORY_SEPARATOR;
+		    $basePath = $basePath . $id . DIRECTORY_SEPARATOR;
+		    mkdir( $basePath , 0700 );
+
+		    $uploadPath = $basePath . $_FILES[ 'file' ][ 'name' ];
+
+		    move_uploaded_file( $tempPath, $uploadPath );
+
+		    $answer = array( 'answer' => 'File transfer completed' );
+
+		    $json = json_encode( $answer );
+
+		    echo $json;
+		}
+		else
+		{
+		    echo 'No files';
+		}
+
+	}
+
+	public function get()
+	{
+		  echo 'GET';
+	}
+	
+}
